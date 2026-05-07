97e493b85cb15a6535555552d55ee6bd8f8dbb37
FranGulino <gulinofranco5@gmail.com>
2026-05-07 16:30:03 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "WeShuttle - Rider App",
-  description: "Tu transporte universitario",
+  description: "Tu transporte universitario.",
 };
 
 export default function RootLayout({
