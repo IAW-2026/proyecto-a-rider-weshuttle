7c28e03cdf1bf0c3a6c2c6b0460bb9228bce1267
Franco Gulino <gulinofranco5@gmail.com>
2026-05-11 22:25:47 -0300

    fix: ruteo de Clerk y configuracion de sign in

--- proxy.ts
+++ proxy.ts
@@ -1,17 +1,18 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)', 
-  '/sign-up(.*)', 
-  '/'
-])
+// Esta es la forma más agresiva de decirle que estas rutas son LIBRES
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware(async (auth, request) => {
+  // Si la ruta NO empieza con /sign-in o /sign-up, entonces pedí login
   if (!isPublicRoute(request)) {
-    await auth.protect()
+    await auth.protect();
   }
-})
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!|on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
